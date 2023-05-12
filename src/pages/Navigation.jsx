0d8d9cc1e0e8fb31f1ec0af0986cfe4a93aaa5f5
nGeorgit <n.georgitsogiannis@gmail.com>
2023-05-13 00:48:08 +0300

    got to show custom map but warped

---
+++ src/pages/Navigation.jsx
@@ -0,0 +1,59 @@
+import {
+    Box,
+    Button,
+    Select,
+    Stack,
+    useColorModeValue,
+  } from "@chakra-ui/react";
+  import { MapContainer, TileLayer, useMap, ImageOverlay } from 'react-leaflet'
+  import { useState, useRef } from "react";
+  //import { mapData } from "../assets/mapData.js";
+  //import MapCords from "../components/MapCords";
+  import { useEffect } from "react";
+  //import maps from '../maps/0.png'
+  import { bounds, map, CRS } from "leaflet";
+
+  
+
+  function test() {
+    console.log('sad')
+    return (
+      <h1>Hello</h1>
+    );
+  }
+  
+  function NavPage() {
+    const [title, setTitle] = useState("");
+    const [value, setValue] = useState("");
+    const [sampleObject, setSampleObject] = useState([]);
+
+    const handleChange = (e) => {
+      setValue(e.target.value);
+      setTitle("");
+      document.getElementById("title").value = "default";
+    };
+  
+
+
+    var imageOv = useRef()
+    var mapRef = useRef()
+    const imageBounds = [[0,0],[1943, 2907]]
+    
+    useEffect(() => {
+      if (mapRef.current) {
+        mapRef.current.fitBounds(imageBounds);
+      }
+    }, imageBounds);
+
+    return (
+      <Box align="center" marginTop="1em" fontFamily="Syne">
+        <Button onClick={test}>Tests</Button>
+        <MapContainer center={[500,500]} zoom={1} ref={mapRef} style={{height:500+'px'}}>
+          <ImageOverlay url={"maps/0.png"} bounds={imageBounds} ref={imageOv} ></ImageOverlay>
+        </MapContainer>
+      </Box>
+    );
+  }
+  
+  export default NavPage;
+  
